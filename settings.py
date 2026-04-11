3a09859a84b23ef4c63edc60181e55dc34be397e
Ryma Djoudad <zenyod85@gmail.com>
2026-04-11 16:31:13 -0400

    Fit to my personal screen as it cut off on the bottom

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings"""
         # Screen settings
-        self.screen_width = 1100
-        self.screen_height = 700
+        self.screen_width = 1000
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
