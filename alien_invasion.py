9b4b1be6e36073c30401ae5271905f9e4c2a9dd6
Ryma Djoudad <zenyod85@gmail.com>
2026-04-11 14:02:31 -0400

    Added gitignore (recommended practice), created settings file and utilized files from it

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -8,12 +9,13 @@ class AlienInvasion:
         """Initialize the game and create game resources"""
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1100, 700))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_height, self.settings.screen_width)
+            )
         pygame.display.set_caption("Alien Invasion")
 
-        # Set the background color
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop of the game"""
@@ -24,7 +26,7 @@ class AlienInvasion:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop 
-            self.screen.fill(self.bg_color)        
+            self.screen.fill(self.settings.bg_color)        
             
             # Make the most recently drawn screen visible
             pygame.display.flip()
