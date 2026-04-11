697b0ed58377fc019e75f6659289ede1053bf631
Ryma Djoudad <zenyod85@gmail.com>
2026-04-11 14:04:47 -0400

    Fixed screen dimensions, bg color

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings:
         # Screen settings
         self.screen_width = 1100
         self.screen_height = 700
-        self.bg_color(230, 230, 230)
+        self.bg_color = (230, 230, 230)
