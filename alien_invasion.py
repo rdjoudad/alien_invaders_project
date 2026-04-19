711bba1279afe4fe5b4751f70c27853dd0b0f814
Ryma Djoudad <zenyod85@gmail.com>
2026-04-19 10:41:40 -0400

    Pulling upstream

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,6 +12,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
+
 
 
 class AlienInvasion:
@@ -32,6 +34,9 @@ class AlienInvasion:
 
         self.ship = Ship(ai_game=self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
 
     def run_game(self):
@@ -95,13 +100,20 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+    
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # Make an alien.
+        alien = Alien(self)
+        self.aliens.add(alien)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()  
-        self.ship.blitme() 
+        self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
