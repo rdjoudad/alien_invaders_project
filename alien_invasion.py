ce348ced8020f670afcec7f9f8e1b7d8e4a51c5f
Ryma Djoudad <zenyod85@gmail.com>
2026-04-19 17:28:49 -0400

    Active game elements

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,14 +43,17 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+        # Start Alien Invasion in an active state.
+        self.game_active = True
 
     def run_game(self):
         """Start the main loop of the game"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -81,23 +84,30 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
+        if self.stats.ships_left > 0:
         # Decrement ships_left.
-        self.stats.ships_left -= 1
+            self.stats.ships_left -= 1
 
         # Get rid of any remaining bullets and aliens.
-        self.bullets.empty()
-        self.aliens.empty()
+            self.bullets.empty()
+            self.aliens.empty()
 
         # Create a new fleet and center the ship.
-        self._create_fleet()
-        self.ship.center_ship()
+            self._create_fleet()
+            self.ship.center_ship()
 
         # Pause.
-        sleep(0.5)
+            sleep(0.5)
+
+        else: 
+            self.game_active = False
 
     # Look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
+
+    # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_left()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
