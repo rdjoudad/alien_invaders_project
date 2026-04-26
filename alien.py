dde4cf60edff2dd26d1de4519bb28d1ea8c2aead
Ryma Djoudad <zenyod85@gmail.com>
2026-04-26 12:00:37 -0400

    Modified license and transformed to .bmp format

--- alien.py
+++ alien.py
@@ -11,7 +11,7 @@ class Alien(Sprite):
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
 
-        self.image = pygame.image.load('images/alien.png')
+        self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
         
         self.x = float(self.rect.x)
